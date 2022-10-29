ac91ab175235a856ec7077bc08b1a67355f4b115
klovaaxel <klovakarlsson@gmail.com>
2022-10-29 21:17:02 +0200

    fix : remove loggin

--- src/reducers/root-reducer.tsx
+++ src/reducers/root-reducer.tsx
@@ -6,7 +6,6 @@ const rootReducer = (
     state = initState,
     action: { type: string; value: any }
 ) => {
-    console.log(state);
     switch (action.type) {
         case "CHANGE_PAGE_TITLE":
             const newState = JSON.parse(JSON.stringify(state));
