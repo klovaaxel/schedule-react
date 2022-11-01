270d27b83714f97d5e1c988bbde2c9340c2097de
klovaaxel <klovakarlsson@gmail.com>
2022-11-01 21:16:01 +0100

    Restructure of app and new pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.scss";
-import App from "./App";
+import App from "./shell/App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
