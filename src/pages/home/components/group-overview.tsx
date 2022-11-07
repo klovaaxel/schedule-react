b7407415327a659e6fed0cf95039b217e7a551ba
klovaaxel <klovakarlsson@gmail.com>
2022-11-07 22:35:41 +0100

    Temp GroupOverview

--- src/pages/home/components/group-overview.tsx
+++ src/pages/home/components/group-overview.tsx
@@ -1,11 +1,21 @@
+import { GroupModel } from "../../../objects/group/group-model";
+
 //import { useTranslation } from "react-i18next";
-const GroupOverview = (group: any) => {
+const GroupOverview = (group: GroupModel) => {
     //const { t } = useTranslation();
 
     return (
         <article>
             <h3>{group.id}</h3>
-            <p>I'm a Group!</p>
+            <ul>
+                {group.courses?.map((course) => {
+                    return (
+                        <li key={course.id}>
+                            <h4>{course.id}</h4>
+                        </li>
+                    );
+                })}
+            </ul>
         </article>
     );
 };
