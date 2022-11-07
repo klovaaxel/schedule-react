126b52a36629106cdba68eac401401b3a1697403
klovaaxel <klovakarlsson@gmail.com>
2022-11-07 21:53:16 +0100

    removed unused import

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import "./i18n";
 
-import { Resolver } from "@stoplight/json-ref-resolver";
 import { Provider } from "react-redux";
 import store from "./store/page-store";
 
