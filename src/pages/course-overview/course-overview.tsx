d46c84ef90e47b3294c6ae6ddc1d04e08d3d1566
klovaaxel <klovakarlsson@gmail.com>
2022-11-13 13:29:40 +0100

    Update Course-overview with new navbar

--- src/pages/course-overview/course-overview.tsx
+++ src/pages/course-overview/course-overview.tsx
@@ -1,28 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 //import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { GetCourse } from "../../objects/course/course-api";
+import { CourseModel } from "../../objects/course/course-model";
 
 const CourseOverview = () => {
     const dispatch = useDispatch();
     //const { t } = useTranslation();
     const { id } = useParams();
+    const [course, setCourse] = useState<CourseModel>();
 
-    async function test() {
-        console.log(await GetCourse("weuweb01"));
-    }
+    useEffect(() => {
+        async function getCourseInfo() {
+            const course = await GetCourse("weuweb01");
+
+            setCourse(course ? course : new CourseModel(null));
+        }
+
+        getCourseInfo();
+    });
 
     useEffect(() => {
         dispatch({
             type: "CHANGE_PAGE",
             value: {
-                currentPageTitle: "course-title",
-                currentPage: "course-id",
+                currentPageTitle: `${course?.name}`,
+                currentPage: `course-${id}`,
             },
         });
-
-        test();
     });
 
     return <h1>Hello world, I am {id}</h1>;
